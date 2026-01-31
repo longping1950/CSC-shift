90a8e2ed138c3204feba2fce89b8327edcb0b655
longping1950 <konhome1110@gmail.com>
2026-01-31 22:54:55 +0800

    v4.2

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = 'csc-shift-v1.1';
+const CACHE_NAME = 'csc-shift-v3';
+// 使用相對路徑 ./ 以相容 GitHub Pages 子目錄
 const urlsToCache = [
   './shift.html',
   './schedule.html',
   './manifest.json',
-  'web/favicon.ico',
-  'web/icon-192.png',
-  'web/icon-512.png',
+  './web/favicon.ico',
+  './web/icon-192.png',
+  './web/icon-512.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
@@ -13,10 +14,17 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  // 強制讓 Service Worker 立即進入 waiting 狀態，不需等待舊的 SW 停止
+  self.skipWaiting();
+  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.error('Cache addAll error: ', err);
       })
   );
 });
@@ -37,14 +45,19 @@ self.addEventListener('fetch', event => {
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    Promise.all([
+      // 清理舊的快取
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      }),
+      // 讓 Service Worker 立即接管所有頁面，不需重新整理
+      self.clients.claim()
+    ])
   );
 });
